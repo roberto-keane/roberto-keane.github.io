995b77994e2a71c14a195591c7cadb368e9bf968
Roberto Keane <robert96keane@gmail.com>
2018-12-28 19:14:10 +0700

    new files

---
+++ precache-manifest.b056ff0daf874fd6ca22e7b508861cbc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "903e0d6e68ce6c5a44b6",
+    "url": "/static/css/main.11713684.chunk.css"
+  },
+  {
+    "revision": "903e0d6e68ce6c5a44b6",
+    "url": "/static/js/main.903e0d6e.chunk.js"
+  },
+  {
+    "revision": "971dccf2c8870c7ba496",
+    "url": "/static/js/1.971dccf2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "765aec863c14b9043e66a59bcf15df88",
+    "url": "/static/media/profile.765aec86.jpg"
+  },
+  {
+    "revision": "e1c279a9fdf9d2a09752094917594689",
+    "url": "/static/media/robofriends.e1c279a9.png"
+  },
+  {
+    "revision": "0c1ef73de7fdf4de4000c8b82eb7f508",
+    "url": "/static/media/faceRecognition.0c1ef73d.png"
+  },
+  {
+    "revision": "2dd79c856c288f2b74941d4d20029fc8",
+    "url": "/index.html"
+  }
+];
